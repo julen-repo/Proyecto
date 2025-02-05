4c837c808fb4ace83a6018f4778035518dcb5860
alejandroIES <alejandro.guerra.sabater@iesjulianmarias.es>
2025-02-05 10:32:22 +0100

    BDD

---
+++ script.sql
@@ -0,0 +1,22 @@
+DROP DATABASE IF EXISTS AlquilaMesas;
+CREATE DATABASE AlquilaMesas;
+
+CREATE TABLE Usuario(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR (255)
+);
+
+CREATE TABLE Mesa(
+	id INT AUTO_INCREMENT PRIMARY KEY
+);
+
+CREATE TABLE MesaAlquilada (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    id_Usuario INT,
+    id_Mesa INT,
+    inicio DATETIME,
+    fin DATETIME,
+    juego VARCHAR (255),
+    FOREIGN KEY (id_Usuario) REFERENCES Usuario(id) ON DELETE SET NULL,
+    FOREIGN KEY (id_Mesa) REFERENCES Mesa(id) ON DELETE SET NULL
+);
