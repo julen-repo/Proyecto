28a7eda4c2062b3eb073c5279d1ad99fb79375ca
alejandroIES <alejandro.guerra.sabater@iesjulianmarias.es>
2025-02-05 10:41:18 +0100

    Datos actualizados

--- script.sql
+++ script.sql
@@ -4,7 +4,11 @@ USE AlquilaMesas;
 
 CREATE TABLE Usuario(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR (255)
+    nombre VARCHAR (255),
+    apellidos VARCHAR (255),
+    dni VARCHAR (255),
+    telefono INT
+
 );
 
 CREATE TABLE Mesa(
@@ -22,8 +26,14 @@ CREATE TABLE MesaAlquilada (
     FOREIGN KEY (id_Mesa) REFERENCES Mesa(id) ON DELETE SET NULL
 );
 
--- Insertar usuarios
-INSERT INTO Usuario (nombre) VALUES ('Carlos Fernández'), ('María López'), ('Luis García'), ('Sofía Martínez'), ('Javier Gómez');
+-- Insertar usuarios con nuevos atributos
+INSERT INTO Usuario (nombre, apellidos, dni, telefono) 
+VALUES 
+('Carlos', 'Fernández Pérez', '12345678A', 666123456),
+('María', 'López García', '98765432B', 665987654),
+('Luis', 'García Sánchez', '45678901C', 678321987),
+('Sofía', 'Martínez Ruiz', '78912345D', 677654321),
+('Javier', 'Gómez Fernández', '32198765E', 612345678);
 
 -- Insertar mesas
 INSERT INTO Mesa () VALUES ();
@@ -36,4 +46,3 @@ INSERT INTO Mesa () VALUES ();
 INSERT INTO Mesa () VALUES ();
 INSERT INTO Mesa () VALUES ();
 INSERT INTO Mesa () VALUES ();
-
